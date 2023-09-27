58cd3b9e1491c8a099432d377f2071a2ded252af
Andriamanitra <mikko77792@gmail.com>
2023-09-27 16:08:43 +0300

    spaces > tabs

--- gotoctags.lua
+++ gotoctags.lua
@@ -9,22 +9,22 @@ local shell = import("micro/shell")
 local micro = import("micro")
 
 function init()
-	config.MakeCommand("jumptag", jumptagCommand, config.NoComplete)
-	config.AddRuntimeFile("gotoctags", config.RTHelp, "help/gotoctags.md")
+    config.MakeCommand("jumptag", jumptagCommand, config.NoComplete)
+    config.AddRuntimeFile("gotoctags", config.RTHelp, "help/gotoctags.md")
 end
 
 function jumptagCommand(bufpane)
-		local filename = bufpane.Buf.Path
-		-- --sort=no shows symbols in file order
-		local cmd = string.format("ctags -f - --sort=no --fields=n '%s' | awk -F':|\\t' '{printf \\\"%%4d %%s\\n\\\", \\$NF, \\$1}' | fzf --reverse", filename)
-		local wait = false
-		local getOutput = true
-		local out = shell.RunInteractiveShell(string.format("bash -c \"%s\"", cmd), wait, getOutput)
-		local linenum = out:match("%d+")
-		if linenum == nil then
-			micro.InfoBar():Message("[gotoctags] jump cancelled")
-			return
-		end
-		bufpane.Cursor.Y = tonumber(linenum) - 1
-		micro.InfoBar():Message("[gotoctags] jumped to line " .. linenum)
+    local filename = bufpane.Buf.Path
+    -- --sort=no shows symbols in file order
+    local cmd = string.format("ctags -f - --sort=no --fields=n '%s' | awk -F':|\\t' '{printf \\\"%%4d %%s\\n\\\", \\$NF, \\$1}' | fzf --reverse", filename)
+    local wait = false
+    local getOutput = true
+    local out = shell.RunInteractiveShell(string.format("bash -c \"%s\"", cmd), wait, getOutput)
+    local linenum = out:match("%d+")
+    if linenum == nil then
+        micro.InfoBar():Message("[gotoctags] jump cancelled")
+        return
+    end
+    bufpane.Cursor.Y = tonumber(linenum) - 1
+    micro.InfoBar():Message("[gotoctags] jumped to line " .. linenum)
 end
